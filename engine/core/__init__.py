061f0941bb94925695c4919aa202093db0e9a2f5
Drew M <urbanbonfire@protonmail.com>
2020-05-31 10:43:07 -0500

    First commit of beta engine

---
+++ engine/core/__init__.py
@@ -0,0 +1,9 @@
+
+from .game import *
+from .mob import *
+from .scene import *
+from .tileset import *
+from .uidialogue import *
+from .uiselect import *
+
+from . import filepaths
---
+++ scripts/entrypoint.py
@@ -0,0 +1,68 @@
+# entrypoint.py
+
+import os
+
+import pygame
+import engine.core
+from engine.core import filepaths
+	
+def newgame_init(game):
+
+	game.load_scene("scene1", os.path.join(filepaths.scene_path, "scene_cottage.tmx"))
+	
+	game.obj_stack = []
+	game.obj_stack.append(game.scene)
+	game.obj_stack.append(game.fader)
+	
+	game.player.facing = "south"
+	game.renderer.following = game.player
+	
+	game.next_script = newgame_loop # eventually newgame will not have a loop
+	game.fader.fade_in()
+	
+def newgame_loop(game):
+
+	game.player.move(game.controller.x_axis, game.controller.y_axis)
+	
+	if game.controller.exit:
+		game.next_script = title_init
+		game.music_tracks["titletrack"].fadeout(1000)
+		game.fader.fade_out()
+
+def title_init(game): # inits always clear game.obj_stack
+
+	game.obj_stack = []
+	game.obj_stack.append(game.title_card)
+	game.obj_stack.append(game.ui["titleselect"])
+	game.obj_stack.append(game.fader)
+		
+	game.ui["titleselect"].start()
+	
+	game.music_tracks["titletrack"].play(-1)
+	game.next_script = title_loop
+	game.fader.fade_in()
+	
+def title_loop(game):
+
+	if game.ui["titleselect"]._returned:
+		if game.ui["titleselect"].value == 0: # New Game
+			game.next_script = newgame_init
+			game.fader.fade_out()			
+		elif game.ui["titleselect"].value == 1: # Quit to Desktop
+			game.music_tracks["titletrack"].fadeout(1000)
+			game.next_script = game.exit
+			game.fader.fade_out()
+		game.ui["titleselect"].visible = False
+
+def init():
+
+	pygame.init()
+	game = engine.core.Game(os.path.join(filepaths.image_path, "titlecard.png"))
+	game.ui["dialoguebox"] = engine.core.UI_Dialogue("dialoguebox", game, (170,360), (300,100))
+	game.ui["titleselect"] = engine.core.UI_Select("titleselect", game, (245,300), (150,54), ["New Game", "Quit to Desktop"])
+	game.player = engine.core.Mob(game, os.path.join(filepaths.image_path, "sprite_jon.png"), "Jon")
+	game.music_tracks["titletrack"] = pygame.mixer.Sound(os.path.join(filepaths.sound_path, "rstheme.ogg"))
+	
+	title_init(game)
+	game.main()
+
